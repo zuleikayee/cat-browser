07750a2d53ac26c99138ab6f516ff5a3f732af23
Zuleika Yee <zuleikayee@gmail.com>
2022-11-20 23:45:44 -0800

    use MUI components, hooks for Grid, TODO: pagination

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import './App.css';
-import useApiData from './utils/useApiData';
-import DropdownMenu from './components/DropdownMenu/DropdownMenu';
-import Grid from './components/Grid/Grid';
-import Button from '@mui/material/Button';
+import CatBrowser from './components/CatBrowser/CatBrowser';
 
-function App() {  
-  const breeds = useApiData();
-  const breedNameArr = [];
-
-  for (const curr of breeds) {
-    const { name } = curr;
-    breedNameArr.push(name);
-  }
-
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Cat Browser</h1>
-        <DropdownMenu />
-        <Grid />
-        <Button variant="contained">Load More</Button>
-      </header>
-    </div>
+    <CatBrowser />
   );
 }
 
